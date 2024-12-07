b1c8a90f051142f4526caa3c8fa002e7d079cdfc
cjimenezm5 <cjimenezm.ieu2023@student.ie.edu>
2024-12-07 16:39:14 +0100

    First modification

--- planner.py
+++ planner.py
@@ -6,7 +6,7 @@ def load_from_JSON(filename):
     filepath = "Data/" + filename + ".json"
 
     if not os.path.exists("Data/" + filename + ".json"):
-        print(f"File '{"Data/" + filename + ".json"}' does not exist.")
+        print("File Data/" + filename + ".json does not exist.")
         return []
 
     try:
@@ -14,7 +14,7 @@ def load_from_JSON(filename):
         with open("Data/" + filename + ".json", "r") as f:
             return json.load(f)  
     except json.JSONDecodeError:
-        print(f"Error: The file '{"Data/" + filename + ".json"}' contains invalid JSON.")
+        print("Error: The file Data/" + filename + ".json contains invalid JSON.")
         return []
     
 # Study hours = number of hours the student plans in working per day
